1b6db0a197caef2dbe4f3d41682bec400fa1cec2
Rishi <rishisr.9044@gmail.com>
2022-09-25 12:19:46 +0530

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
 import Video from './Component/Video';
 import Header from './Component/Header';
 import Option from './Component/Option';
 import Notification from './Component/Notification';
 
+import './App.css';
 function App() {
   return (
     <div className='app' >
